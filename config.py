6d8c97e09c8837eaec50fcd4afcc9097f58aa88d
tkodimac <tkmachiwenyika@gmail.com>
2024-08-26 17:45:02 +0000

    install all the required Python packages installed

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://username:password@localhost/water_products_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://gitpod:@localhost/water_products_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
