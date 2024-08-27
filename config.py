8ab5aeaef947b57e2b877fb36d7ec3ff3cc37d99
tkodimac <tkmachiwenyika@gmail.com>
2024-08-27 11:22:52 +0000

    config database and additional features

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://username:password@localhost/water_product_review'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://keviny:rebecca@localhost/water_product_review'
+    ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
