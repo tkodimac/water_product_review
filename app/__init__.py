8d0c8112840904436d92c515dbc8eae8f6b4a5de
tkodimac <tkmachiwenyika@gmail.com>
2024-08-27 13:15:54 +0000

    add os

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -12,17 +14,18 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
     if os.environ.get("DEVELOPMENT") == "True":
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
     else:
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
         uri = os.environ.get("DATABASE_URL")
         if uri.startswith("postgres://"):
             uri = uri.replace("postgres://", "postgresql://", 1)
-        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
-    app.config['SECRET_KEY'] = '62c_windmill_street'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://keviny:rebecca@localhost/water_product_review'
+    
+
+
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
