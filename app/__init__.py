e3320853dc909ea7111efa6f5f724e33f831a653
tkodimac <tkmachiwenyika@gmail.com>
2024-08-27 11:29:17 +0000

    modify local server settings

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,8 @@ login_manager.login_message_category = 'info'
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/water_product_review'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://keviny:rebecca@localhost/water_product_review'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
     bcrypt.init_app(app)
