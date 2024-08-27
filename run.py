37eadecc474962053aa28203cdb0ef3b68cbf865
tkodimac <tkmachiwenyika@gmail.com>
2024-08-27 11:54:31 +0000

    add email validator

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()  # This creates the tables if they do not exist
 
 if __name__ == "__main__":
     app.run(debug=True)
