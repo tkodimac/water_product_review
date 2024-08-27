3acdd839c487ecbaf47754db08a5354896f632af
tkodimac <tkmachiwenyika@gmail.com>
2024-08-27 15:08:40 +0000

    change settings

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ os.environ.setdefault("DATABASE_URL", "postgresql:///taskmanager")
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '62c_windmill_street'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://keviny:rebecca@localhost/water_product_review'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://keviny:rebecca@localhost/water_product_review.db'
 
 
 
