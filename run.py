acec95e639ff2458980980619d41055284478983
tkodimac <tkmachiwenyika@gmail.com>
2024-08-28 22:44:16 +0000

    Bind Flask app to 0.0.0.0 and dynamic port

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
+import os
 from app import create_app, db
+from app import app  # Make sure this imports your Flask app correctly
 
 
-app = create_app()
+#app = create_app()
 
-with app.app_context():
-    db.create_all()  # This creates the tables if they do not exist
+#with app.app_context():
+#    db.create_all()  # This creates the tables if they do not exist
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
