15c73168924fe92d33b8a51d92d5e9bd9f328e9f
tkodimac <tkmachiwenyika@gmail.com>
2024-08-28 17:42:46 +0000

    adjust run.py to fix page not loading

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from app import create_app, db
 
 app = create_app()
 
-#with app.app_context():
-#    db.create_all()  # This creates the tables if they do not exist
+with app.app_context():
+    db.create_all()  # This creates the tables if they do not exist
 
 if __name__ == "__main__":
     app.run(
